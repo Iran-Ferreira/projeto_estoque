c5d7e65cfed7d9e31d62d544c7dbd72db42a20e3
Iran Ferreira <iranferreira712@gmail.com>
2024-11-25 19:09:19 -0300

    feat: changing js files to ts

--- API/routes/login.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const authController = require('../controllers/authController.js');
-
-const routes = new Router();
-routes.post("/login", authController.logar);
-routes.post("/loginADM", authController.logar);
-
-module.exports = routes;
